ed31a5dce7eee7870cd855f805f8c546f151e5d0
Matt Weichel <maweiche@gmail.com>
2021-09-29 17:16:57 -0700

    added current time updater to js

--- Develop/app.js
+++ Develop/app.js
@@ -1,2 +1,12 @@
 //Current date
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+//Current time
+let updateTime = function () {
+   let currentTime = moment().format('h:mm:ss');
+   $("#1").text(currentTime)
+}
+setInterval(updateTime, 1000);
+
+
+
