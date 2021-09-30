fb14be6c311f4e8ca75bb97dad4855a7d1fe6681
Matt Weichel <maweiche@gmail.com>
2021-09-30 16:50:47 -0700

    timestamp is fixed, time looks beter, margin on text area removed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //Current date
 let updateDate = function () {
     let currentDate = moment().format("dddd, MMMM Do YYYY");
-    $("#currentDay").text(currentDate)
+    $("#currentDay").text(currentDate);
 }
 //Current time
 let updateTime = function () {
    let currentTime = moment().format('h:mm:ss');
-   $("#1").text(currentTime)
+   $("#timestamp").text(currentTime);
 }
 
 
@@ -14,65 +14,65 @@ let updateTime = function () {
 var cDay = [
     {
         id: "0",
-        hour: "09",
+        hour: "9",
         time: "09",
-        ampm: "am",
+        ampm: "AM",
         task: "",
     },
     {
         id: "1",
         hour: "10",
         time: "10",
-        ampm: "am",
+        ampm: "AM",
         task: "",
     },
     {
         id: "2",
         hour: "11",
         time: "11",
-        ampm: "am",
+        ampm: "AM",
         task: "",
     },
     {
         id: "3",
         hour: "12",
         time: "12",
-        ampm: "pm",
+        ampm: "PM",
         task: "",
     },
     {
         id: "4",
-        hour: "01",
+        hour: "1",
         time: "13",
-        ampm: "pm",
+        ampm: "PM",
         task: "",
     },
     {
         id: "5",
-        hour: "02",
+        hour: "2",
         time: "14",
-        ampm: "pm",
+        ampm: "PM",
         task: "",
     },
     {
         id: "6",
-        hour: "03",
+        hour: "3",
         time: "15",
-        ampm: "pm",
+        ampm: "PM",
         task: "",
     },
     {
         id: "7",
-        hour: "04",
+        hour: "4",
         time: "16",
-        ampm: "pm",
+        ampm: "PM",
         task: "",
     },
     {
         id: "8",
-        hour: "05",
+        hour: "5",
         time: "17",
-        ampm: "pm",
+        ampm: "PM",
         task: "",
     }
 ]
@@ -94,9 +94,6 @@ function init () {
 
     if (storedCDay) {
         cDay = storedCDay
-    } else {
-        alert("Nothing to save!")
-        return;
     }
     saveText();
     displayText();
@@ -119,7 +116,7 @@ cDay.forEach(function(cHour) {
 
     //creates text area for schedule
     var hrToDo = $("<section>")
-        .attr({"class": "col-md-9 description p-0"});
+        .attr({"class": "col-md-8 description p-0"});
     var txtData =$("<textarea>");
     
     hrToDo.append(txtData);
@@ -141,7 +138,7 @@ cDay.forEach(function(cHour) {
     ///creates the save button using an i tag and logo, and a button tag to append the i class to
     var saveButton = $("<i class = 'fa fa-save fa-lg'></i> ")
     var saveToDo = $("<button>")
-        .attr({"class": "col-md saveBtn"});
+        .attr({"class": "col-md-2 saveBtn"});
 
     ///Append everything to page!!!!
     saveToDo.append(saveButton);
